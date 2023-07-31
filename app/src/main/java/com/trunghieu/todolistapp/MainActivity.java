ec43d9785b33cc7791b6a4b29b9b7fa96781eb39
Haunguyen42193 <haunguyen42193@gmail.com>
2023-07-31 19:51:07 +0700

    add-table-task

--- app/src/main/java/com/trunghieu/todolistapp/MainActivity.java
+++ app/src/main/java/com/trunghieu/todolistapp/MainActivity.java
@@ -3,20 +3,28 @@ package com.trunghieu.todolistapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.trunghieu.todolistapp.data.DBHelper;
+import com.trunghieu.todolistapp.model.Task;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private Button btnSignup;
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dbHelper = new DBHelper(this);
+        dbHelper.insertTask(new Task("Hau", "Nguyen", "Van", "2002", 1));
         btnLogin = findViewById(R.id.btn_main_login);
         btnSignup = findViewById(R.id.btn_main_signup);
 
---
+++ app/src/main/java/com/trunghieu/todolistapp/model/Task.java
@@ -0,0 +1,74 @@
+package com.trunghieu.todolistapp.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.UUID;
+
+@Entity(tableName = "Tasks")
+public class Task {
+    @PrimaryKey
+    private String id;
+    private String title;
+    private String description;
+    private String createDate;
+    private String completed;
+    private int userId;
+
+    public Task(String title, String description, String createDate, String completed, int userId) {
+        this.id = UUID.randomUUID().toString();
+        this.title = title;
+        this.description = description;
+        this.createDate = createDate;
+        this.completed = completed;
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public String getCompleted() {
+        return completed;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public void setCompleted(String completed) {
+        this.completed = completed;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}

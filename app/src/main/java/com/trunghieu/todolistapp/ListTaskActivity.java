8a48f8f90ae3601cc81d6ff72583153e6b90bcda
Haunguyen42193 <haunguyen42193@gmail.com>
2023-08-02 01:57:33 +0700

    task-list-show

--- app/src/main/java/com/trunghieu/todolistapp/ListTaskActivity.java
+++ app/src/main/java/com/trunghieu/todolistapp/ListTaskActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.trunghieu.todolistapp.adapter.TaskAdapter;
 import com.trunghieu.todolistapp.data.DBHelper;
@@ -24,6 +25,17 @@ public class ListTaskActivity extends AppCompatActivity {
         dbHelper = new DBHelper(this);
         recyclerView = (RecyclerView) findViewById(R.id.rcvTaskListAdmin);
         listTask = dbHelper.getTaskData();
+        for (Task task : listTask) {
+            Log.d("ListTaskActivity", "Task ID: " + task.getId());
+            Log.d("ListTaskActivity", "Task Title: " + task.getTitle());
+            Log.d("ListTaskActivity", "Task Description: " + task.getDescription());
+            Log.d("ListTaskActivity", "Task Created: " + task.getCreateDate());
+            Log.d("ListTaskActivity", "Task Completed: " + task.getCompleted());
+            Log.d("ListTaskActivity", "Task User ID: " + task.getUserId());
+            Log.d("ListTaskActivity", "Task Category ID: " + task.getCategoryID());
+        }
+
+
         adapter = new TaskAdapter(listTask, this);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
